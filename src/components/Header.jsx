6cba5538765369b8236eb216dd4e848506dc4799
Yushenko Dima <damios.1812@gmail.com>
2025-09-11 08:57:05 +0200

    added new component 'search'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div>
             <nav className="red darken-1">
                 <div className="nav-wrapper">
-                    <Link to="/" className="brand-logo">React Movies</Link>
+                    <Link to="/" className="brand-logo">React Shop</Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/contacts">Contacts</Link></li>                     
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,46 @@
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { getAllCategoris } from '../api';
 import { Preloader } from './Preloader';
 import { CategoryList } from '../components/CatarogyList';
+import { Search } from '../components/Search';
 
 function Home() {
     const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalod] = useState([]);
+
+    const { pathname, search } = useLocation();
+    const navigate = useNavigate();
+
+    const handleSearch = (str) => {
+        setFilteredCatalod(
+            catalog.filter((item) =>
+                item.strCategory.toLowerCase().includes(str.toLowerCase())
+            ),
+            navigate(`${pathname}?search=${str}`)
+        );
+    };
 
     useEffect(() => {
         getAllCategoris().then((data) => {
             setCatalog(data.categories);
+            setFilteredCatalod(search ?
+                data.categories.filter((item) =>
+                    item.strCategory
+                        .toLowerCase()
+                        .includes(search.split('=')[1].toLowerCase())
+                ) : data.categories
+            );
         });
-    }, []);
+    }, [search]);
 
     return (
         <>
+            <Search cb={handleSearch}></Search>
             {!catalog.length ? (
                 <Preloader />
             ) : (
-                <CategoryList catalog={catalog} />
+                <CategoryList catalog={filteredCatalog} />
             )}
         </>
     );
